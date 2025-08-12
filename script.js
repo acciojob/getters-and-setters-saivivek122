f55913100e115c2f764dec21f7355a0d10c08541
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-12 07:39:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name=name;
+		this._age=age;
+	}
+	get name(){
+		return this._name;
+	}
 
-class Student extends Person {}
+	set age(age){
+		this._age=age;
+	}
 
-class Teacher extends Person {}
+	get age(){
+		return this._age;
+	}
+}
+
+class Student extends Person {
+	constructor(name){
+		super(name)
+	}
+	study(){
+		return this.name+"is studying"
+	}
+}
+
+class Teacher extends Person {
+	constructor(name){
+		super(name)
+	}
+
+	teach(){
+		return this.name+"is teaching"
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
